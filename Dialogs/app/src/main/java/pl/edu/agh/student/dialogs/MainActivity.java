6c957f8d04cd803a3588bacf13bef28999f828ce
AndDialogMenu <uoc00865@dsiay.com>
2016-12-07 20:31:33 +0100

    multiple-choice list

--- Dialogs/app/src/main/java/pl/edu/agh/student/dialogs/MainActivity.java
+++ Dialogs/app/src/main/java/pl/edu/agh/student/dialogs/MainActivity.java
@@ -6,7 +6,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    ArrayList mSelectedItems;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,21 +18,46 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void open(View view) {
+        mSelectedItems = new ArrayList();  // Where we track the selected items
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
-        builder.setMessage(R.string.dialog_text);
+        // Set the dialog title
+        builder.setTitle(R.string.dialog_title);
 
-        builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+        // Specify the list array, the items to be selected by default (null for none),
+        // and the listener through which to receive callbacks when items are selected
+        builder.setMultiChoiceItems(R.array.choices, null,
+                new DialogInterface.OnMultiChoiceClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which,
+                                        boolean isChecked) {
+                        if (isChecked) {
+                            // If the user checked the item, add it to the selected items
+                            mSelectedItems.add(which);
+                        } else if (mSelectedItems.contains(which)) {
+                            // Else, if the item is already in the array, remove it
+                            mSelectedItems.remove(Integer.valueOf(which));
+                        }
+                    }
+                });
+
+
+
+        // Set the action buttons
+        builder.setPositiveButton(R.string.action_button_2, new DialogInterface.OnClickListener() {
+            @Override
             public void onClick(DialogInterface dialog, int id) {
-                //onClick() OK
+                // User clicked OK, so save the mSelectedItems results somewhere
+                // or return them to the component that opened the dialog
             }
         });
 
-        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+        builder.setNegativeButton(R.string.action_button_1, new DialogInterface.OnClickListener() {
+            @Override
             public void onClick(DialogInterface dialog, int id) {
-                //onClick() Cancel
             }
         });
+
 
         AlertDialog alertDialog = builder.create();
         alertDialog.show();
